278c96209113590b51ef5db514f4322a79356be8
mikeanan <chennanbang@gmail.com>
2016-07-17 22:17:00 +0800

    Route 修改及功能的定義 fix error not showed bug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,26 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * 顯示所有任務
+ */
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+
+/**
+ * 增加新的任務
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * 刪除任務
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });

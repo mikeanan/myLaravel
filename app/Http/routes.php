dc790f372a819c681854f045894a59c29fa77c41
mikeanan <chennanbang@gmail.com>
2016-07-17 23:46:50 +0800

    輸入驗證及錯誤顯示

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,7 +25,17 @@ Route::get('/', function () {
  * 增加新的任務
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    return redirect('/');
 });
 
 /**

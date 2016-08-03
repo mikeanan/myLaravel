bccde63fab992ab482e9e2a3483674662ecf6202
mikeanan <chennanbang@gmail.com>
2016-08-03 13:15:45 +0800

    測試 Route，Controller，View 傳參數

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get("/","myResourceController@index");
+
+Route::get("/show/{name}","myResourceController@show");
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 - 哈囉，{{$name_from_server}}</div>
             </div>
         </div>
     </body>

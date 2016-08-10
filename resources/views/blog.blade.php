23676f123c1203f5ea4c8be4e6d55e567a57f4f8
mikeanan <chennanbang@gmail.com>
2016-08-10 10:33:54 +0800

    文章列表 連結到 單篇文章

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -36,7 +36,7 @@
                                 <img src="{{asset("images/blog/blog-one.jpg")}}" alt="">
                             </a>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                            <a  class="btn btn-primary" href="">Read More</a>
+                            <a  class="btn btn-primary" href="{{url("/blog/post/1")}}">Read More</a>
                         </div>
                         <div class="single-blog-post">
                             <h3>Girls Pink T Shirt arrived in store</h3>
@@ -58,7 +58,7 @@
                                 <img src="{{asset("images/blog/blog-two.jpg")}}" alt="">
                             </a>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                            <a  class="btn btn-primary" href="">Read More</a>
+                            <a  class="btn btn-primary" href="{{url("/blog/post/2")}}">Read More</a>
                         </div>
                         <div class="single-blog-post">
                             <h3>Girls Pink T Shirt arrived in store</h3>
@@ -80,7 +80,7 @@
                                 <img src="{{asset("images/blog/blog-three.jpg")}}" alt="">
                             </a>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                            <a  class="btn btn-primary" href="">Read More</a>
+                            <a  class="btn btn-primary" href="{{url("/blog/post/3")}}">Read More</a>
                         </div>
                         <div class="pagination-area">
                             <ul class="pagination">

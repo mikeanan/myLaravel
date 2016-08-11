959e4d36e55508bdf31916e7908cb8c80fb5d6df
mikeanan <chennanbang@gmail.com>
2016-08-11 15:31:30 +0800

    Seeder 集合，執行前會清空資料表內容

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table("products")->truncate();
+        DB::table("categories")->truncate();
+        DB::table("brands")->truncate();
+
+        $this->call(ProductsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(BrandsTableSeeder::class);
     }
 }

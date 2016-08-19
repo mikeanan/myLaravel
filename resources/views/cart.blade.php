69a507c886606570522ba625ac40cb5ee15ca9ac
mikeanan <chennanbang@gmail.com>
2016-08-20 01:10:41 +0800

    修正總金額顯示

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -126,7 +126,6 @@
                             <li>Cart Sub Total <span>$59</span></li>
                             <li>Eco Tax <span>$2</span></li>
                             <li>Shipping Cost <span>Free</span></li>
-                            <li>Total <span>$61</span></li>
                             <li>Total <span>{{ShoppingCart::total()}}</span></li>
                         </ul>
                         <a class="btn btn-default update" href="">Update</a>

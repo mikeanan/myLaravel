5c54af28ff0c465c74fcd0b479bbe82333f62b01
mikeanan <chennanbang@gmail.com>
2016-08-29 22:32:08 +0800

    存放 社群使用者 資訊，可與 users 關聯

---
+++ database/migrations/2016_08_22_072922_create_socialite_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialiteUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socialite_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("vendor");
+            $table->string("vendor_user_id");
+            $table->unsignedInteger("user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('socialite_users');
+    }
+}

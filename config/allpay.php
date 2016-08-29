070cd975d0e97fa0915c16e74cf8237c732318ee
mikeanan <chennanbang@gmail.com>
2016-08-30 00:23:01 +0800

    正式付費的設定

---
+++ config/allpay.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'ServiceURL' => 'https://payment.allpay.com.tw/Cashier/AioCheckOut',
+    'HashKey'    => '改成你的',
+    'HashIV'     => '改成你的',
+    'MerchantID' => '改成你的',
+
+];
